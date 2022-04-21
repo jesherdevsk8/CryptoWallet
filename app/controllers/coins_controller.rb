9ea9630196ce8442851e46d4bfbcecf88dcad029
Jesher Minelli <jesheralves@claretiano.edu.br>
2022-04-21 12:58:37 -0300

    mostra tipo de mineração no edit

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -2,6 +2,7 @@ class CoinsController < ApplicationController
   layout "adm" # criei adm.html.erb em layouts
 
   before_action :set_coin, only: %i[ show edit update destroy ]
+  before_action :set_mining_type_options, only: %i[ show edit update destroy ]
 
   # GET /coins or /coins.json
   def index
@@ -60,6 +61,12 @@ class CoinsController < ApplicationController
   end
 
   private
+
+    def set_mining_type_options
+      # @mining_type_options = MiningType.all.map { |m| [ m.description, m.id ] }
+      @mining_type_options = MiningType.all.pluck(:description, :id)
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_coin
       @coin = Coin.find(params[:id])
@@ -67,6 +74,11 @@ class CoinsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def coin_params
-      params.require(:coin).permit(:description, :acronym, :url_image)
+      params.require(:coin).permit(
+        :description,
+        :acronym,
+        :url_image,
+        :mining_type_id
+      )
     end
 end
--- app/models/coin.rb
+++ app/models/coin.rb
@@ -1,3 +1,11 @@
 class Coin < ApplicationRecord
-    belongs_to :mining_type
+    belongs_to :mining_type #, optional: true
+
+    # métodos .map e .pluck
+
+    # c = Coin.all
+    # c.map { |coin| [coin.description, coin.acronym] } #Retorna descrição e sigla do array coin
+    # c.map(&:description) #Retorna somente descrição e não aceita mais de uma
+    # c.pluck(:description, :acronym) #Aceita retornar mais de um opção
+    
 end

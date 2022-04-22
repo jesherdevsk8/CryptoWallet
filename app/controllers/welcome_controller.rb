83472116c25154326e11374b4b1822296abc5c00
Jesher Minelli <jesheralves@claretiano.edu.br>
2022-04-22 18:23:15 -0300

    aplicação com idioma ingles ou portugues

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,14 @@
 class WelcomeController < ApplicationController
   def index
+    cookies[:curso] = "Curso de Ruby on Rails - [COOKIE]"
+    session[:curso] = "Curso de Ruby on Rails - [SESSION]"
+    
     @meu_nome = params[:nome]
     
     if @meu_nome.nil?
       @meu_nome = "Jesher Minelli Alves"
     end
 
-    @curso = "Ruby on Rails"
+    @curso = params[:curso]
   end
 end
